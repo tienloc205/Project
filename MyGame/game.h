2c1556ce2f4daccdbe3d7d91cca8f013f77e1829
tienloc205 <160756312+tienloc205@users.noreply.github.com>
2024-05-12 16:58:36 +0700

    Add files via upload

---
+++ MyGame/game.h
@@ -0,0 +1,11 @@
+//#ifndef GAME_H_INCLUDED
+//#define GAME_H_INCLUDED
+//
+//
+//#include "function.h"
+//#include "object.h"
+//#include "init.h"
+//
+//void GameLoop();
+//
+//#endif // GAME_H_INCLUDED
